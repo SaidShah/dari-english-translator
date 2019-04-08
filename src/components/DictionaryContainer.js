bb2f848b14163f13e9db4cf2bad2c3541b032e08
said shah <saidmysr20@aol.com>
2019-04-08 17:17:44 -0400

    added test data to database and fixed input boxes css

--- src/components/DictionaryContainer.js
+++ src/components/DictionaryContainer.js
@@ -8,7 +8,6 @@ const config ={
    authDomain: process.env.REACT_APP_DARI_AUTH_DOMAIN,
    databaseURL: process.env.REACT_APP_DARI_DATABASE_URL,
    projectId: process.env.REACT_APP_DARI_PROJECT_ID,
-   storageBucket: process.env.REACT_APP_DARI_STORAGE_BUCKET,
    messagingSenderId: process.env.REACT_APP_DARI_MESSAGING_SENDER_ID
 }
 firebase.initializeApp(config)
@@ -47,7 +46,7 @@ class DictionaryContainer extends Component {
        </div>
        <div className="input-container">
        <h2 className="output-label">Translation</h2>
-        <textarea className="outputform" readOnly={true} id="translation"/>
+        <textarea className="outputform"  id="translation" readOnly={true}/>
        </div>
       </div>
     );
@@ -57,15 +56,20 @@ class DictionaryContainer extends Component {
 
 export default DictionaryContainer;
 
-function translateDari(givenTerm){
-  console.log(process.env.REACT_APP_DARI_API_KEY, 'API KEY')
-  firebase.database().ref("daridictionary/").set({
-    word: "HELLO",
-    dari: `${givenTerm}`
-  })
-
+function translateDari(englishTerm){
+  // firebase.database().ref('englishwords/'+englishTerm).set({
+  //   translation: `${givenDari}`
+  // });
+   firebase.database().ref('/englishwords/' + englishTerm).once('value').then(function(snapshot) {
+     document.getElementById('translation').value=snapshot.val().translation
+  });
 }
 
-function translateEnglish(givenTerm){
-
+function translateEnglish(givenDari){
+  // firebase.database().ref('englishwords/خدا حافظ').set({
+  //   translation: `goodbye`
+  // });
+  firebase.database().ref('/englishwords/'+givenDari).once('value').then(function(snapshot) {
+    document.getElementById('translation').value=snapshot.val().translation
+ });
 }
