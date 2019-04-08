6bf2ecea968d611107fdf230075bd4e7d2d29899
said shah <saidmysr20@aol.com>
2019-04-08 11:30:20 -0400

    added translation window and config vars

--- src/components/DictionaryContainer.js
+++ src/components/DictionaryContainer.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-const firebase = require("firebase");
+import firebase from 'firebase/app';
+import 'firebase/database';
+
 
 class DictionaryContainer extends Component {
   state={
@@ -8,17 +10,34 @@ class DictionaryContainer extends Component {
     givenInput: ''
   }
 
+  handleClick=(givenTerm,language)=>{
+    if(language==="toDari"){
+      translateEnglish(givenTerm)
+    }else if(language==="toEnglish"){
+      translateDari(givenTerm)
+    }
+  }
+
+  handleChange=(e)=>{
+    this.setState({givenInput:e.target.value})
+  }
+
   render() {
 
     return (
       <div>
        <div className="dictionary-buttons-container">
-        <button className="btn">Translate To Dari</button>
-        <button className="btn">Translate To English</button>
+        <button className="btn" onClick={()=>this.handleClick(this.state.givenInput,"toDari")}>Translate To Dari</button>
+        <button className="btn" onClick={()=>this.handleClick(this.state.givenInput,"toEnglish")}>Translate To English</button>
        </div>
 
        <div className="input-container">
-        <textarea className="inputform"/>
+       <h2 className="input-label">Your Text</h2>
+        <textarea className="inputform" value={this.state.givenInput} name="givenInput" onChange={this.handleChange}/>
+       </div>
+       <div className="input-container">
+       <h2 className="output-label">Translation</h2>
+        <textarea className="outputform" readOnly={true} id="translation"/>
        </div>
       </div>
     );
@@ -27,3 +46,11 @@ class DictionaryContainer extends Component {
 }
 
 export default DictionaryContainer;
+
+function translateDari(givenTerm){
+
+}
+
+function translateEnglish(givenTerm){
+
+}
